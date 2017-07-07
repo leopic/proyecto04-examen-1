dc46ec12d121193e1e781cd70d7077f778e724ba
Leo Picado <leopic@gmail.com>
2017-07-07 17:57:54 -0600

    Estilos para el salon screen

--- salon-screen.js
+++ salon-screen.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import {
-    Button,
+    TouchableOpacity,
     Text,
     View
 } from 'react-native';
@@ -17,17 +17,39 @@ export default class SalonScreen extends React.Component {
         const { params } = this.props.navigation.state;
         const mesas = params.mesas;
         let mesasList = mesas.map(mesa => {
-            return <Button onPress={() => navigate('Mesa', {
-                mesa: mesa,
-                agregarAlimentoAMesa: params.agregarAlimentoAMesa,
-                removerAlimentoDeMesa: params.removerAlimentoDeMesa
-            })} title={'' + mesa.key} key={mesa.key}/>
+            return <TouchableOpacity key={mesa.key}
+                onPress={() => { navigate('Mesa', {
+                    mesa: mesa,
+                    agregarAlimentoAMesa: params.agregarAlimentoAMesa,
+                    removerAlimentoDeMesa: params.removerAlimentoDeMesa
+                }) }}>
+                <Text style={{
+                    height: 50,
+                    width: 50,
+                    lineHeight: 50,
+                    textAlign: 'center',
+                    margin: 16,
+                    marginBottom: 8,
+                    fontSize: 16,
+                    backgroundColor: 'rgb(50, 70, 255)',
+                    fontWeight: 'bold',
+                    color: '#fff'
+                }}>{mesa.key}</Text>
+            </TouchableOpacity>;
         });
 
         return (
             <View>
-                <Text>Mesas:</Text>
-                {mesasList}
+                <Text style={{
+                    margin: 16,
+                    marginBottom: 0,
+                    fontWeight: 'bold',
+                    fontSize: 16
+                }}>Total de mesas: {mesas.length}</Text>
+                <View style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap'
+                }}>{mesasList}</View>
             </View>
         );
     }
