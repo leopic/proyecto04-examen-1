35f8a020b44bcae825fec7c56ce4ea6af9f5819b
Leo Picado <leopic@gmail.com>
2017-07-07 16:03:16 -0600

    Lista rudimentaria de alimentos para la cocina

--- cocina-screen.js
+++ cocina-screen.js
@@ -10,13 +10,34 @@ export default class CocinaScreen extends React.Component {
     static navigationOptions = {
         title: 'Cocina',
     };
+
     render() {
         const { params } = this.props.navigation.state;
+        const prepararAlimentos = (mesas) => {
+            let alimentos: any[] = [];
+            mesas.forEach(mesa => {
+                mesa.orden.forEach(alimento => {
+                    alimento.mesaKey = mesa.key;
+                    alimentos.push(alimento);
+                });
+            });
+
+            return alimentos;
+        };
+        const alimentosView = prepararAlimentos(params.mesas).map(alimento => {
+            let key = '' + alimento.mesaKey + alimento.descripcion.replace(' ', '');
+            return <View key={key}>
+                    <Text>Mesa: {alimento.mesaKey}</Text>
+                    <Text>Cantidad: {alimento.cantidad}</Text>
+                    <Text>Descripcion: {alimento.descripcion}</Text>
+                    <Text>----</Text>
+                   </View>;
+        });
 
         return (
             <View>
                 <Text>Pendientes:</Text>
-                <Text>Total de mesas: {params.mesas.length}</Text>
+                {alimentosView}
             </View>
         );
     }
