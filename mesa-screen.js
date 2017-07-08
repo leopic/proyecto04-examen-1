62023a42fbcd098ab1d5229bbbf97d6302b71b43
Leo Picado <leopic@gmail.com>
2017-07-07 20:27:50 -0600

    Dandole estilo a la tabla de la mesa

--- mesa-screen.js
+++ mesa-screen.js
@@ -4,31 +4,93 @@ import React from 'react';
 import {
     Text,
     Button,
+    ListView,
     View
 } from 'react-native';
 
 export default class MesaScreen extends React.Component {
-    // Nav options can be defined as a function of the screen's props:
     static navigationOptions = ({ navigation }) => ({
         title: `Mesa #${navigation.state.params.mesa.key}`,
     });
 
+    // ReactNavigation no repinta el UI despues de un `back`, entonces hay que hacerlo manualmente
+    _agregarAlimentoAMesaProxy = (alimento: any, key: number) => {
+      this.props.navigation.state.params.agregarAlimentoAMesa(alimento, key);
+      this.forceUpdate();
+    };
+
+    // ReactNavigation no repinta el UI despues de un `back`, entonces hay que hacerlo manualmente
+    _removerAlimentoDeMesaProxy = (alimento: any, key: number) => {
+        this.props.navigation.state.params.removerAlimentoDeMesa(alimento, key);
+        this.forceUpdate();
+    };
+
     render() {
-        // The screen's current route is passed in to `props.navigation.state`:
         const { params } = this.props.navigation.state;
         const { navigate } = this.props.navigation;
 
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        let dataSource = ds.cloneWithRows(params.mesa.orden);
+
+        let header = () => {
+            return <Text style={{
+                padding: 16,
+                borderBottomWidth: 2,
+                borderBottomColor: '#000'
+            }}>ORDEN</Text>
+        };
+
+        let row = (entrada) => {
+            let tipo = (entrada.tipo[0] + entrada.tipo[1]).toUpperCase();
+
+            return <View style={{
+                padding: 16,
+                paddingTop: 8,
+                borderBottomWidth: 1,
+                borderBottomColor: '#999',
+                alignItems: 'center',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                flexWrap: 'wrap'
+            }}>
+                <Text style={{
+                    borderColor: '#666',
+                    borderWidth: 1,
+                    height: 24,
+                    lineHeight: 24,
+                    marginRight: 8,
+                    textAlign: 'center',
+                    width: 24,
+                }}>{tipo}</Text>
+
+                <Text style={{
+                    borderColor: '#333',
+                    borderWidth: 1,
+                    height: 24,
+                    lineHeight: 24,
+                    marginRight: 8,
+                    textAlign: 'center',
+                    width: 24
+                }}>{entrada.cantidad}</Text>
+                <Text style={{flexGrow: 2}}>{entrada.descripcion}</Text>
+                <Button onPress={() => {
+                    this._removerAlimentoDeMesaProxy(entrada, params.mesa.key);
+                }} title="X" style={{width: 20}}/>
+            </View>
+        };
+
         return (
             <View>
-                <Text>Numero de mesa: {params.mesa.key}</Text>
-                <Text>LISTA DE ALIMENTOS</Text>
-                <Button onPress={() => {
-                    params.removerAlimentoDeMesa(params.mesa.orden[0], params.mesa.key);
-                }} title="Remover Alimento"/>
-                <Button onPress={() => navigate('AgregarAlimento', {
-                    key: params.mesa.key,
-                    agregarAlimentoAMesa: params.agregarAlimentoAMesa
-                })} title="Agregar Alimento"/>
+                <ListView enableEmptySections={true}
+                          dataSource={dataSource}
+                          renderSectionHeader={header}
+                          renderRow={row}/>
+
+                <Button title="Agregar Alimento"
+                        onPress={() => navigate('AgregarAlimento', {
+                            key: params.mesa.key,
+                            agregarAlimentoAMesa: this._agregarAlimentoAMesaProxy
+                        })}/>
             </View>
         );
     }
